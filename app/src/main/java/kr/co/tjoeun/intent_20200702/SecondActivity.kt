8a223938b9de65b1f7893ab6e7bf1eda897bf257
조경진 <kj_cho@idealidea.co.kr>
2020-07-02 20:35:58 +0900

    받아서 사용하는 쪽의 코드 완성

--- app/src/main/java/kr/co/tjoeun/intent_20200702/SecondActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200702/SecondActivity.kt
@@ -2,10 +2,21 @@ package kr.co.tjoeun.intent_20200702
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        들어온 데이터를 받아서 텍스트뷰에 반영
+
+//        액티비티가 가진 intent변수를 통해서 첨부된 (String) 데이터 저장
+        val receivedMessage = intent.getStringExtra("message")
+
+//        저장한 데이터를 텍스트뷰에 반영
+        receivedMessageTxt.text = receivedMessage
+
+
     }
 }
